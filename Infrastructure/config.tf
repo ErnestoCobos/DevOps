5d06a14de689ad5d70e2b579aba58c2309d196fb
Ernesto Cobos <ernesto@cobos.io>
2022-12-14 00:07:50 -0600

    feat: added vultr provider

--- Infrastructure/config.tf
+++ Infrastructure/config.tf
@@ -6,10 +6,22 @@ terraform {
       name = "core-services"
     }
   }
+
+  required_providers {
+    vultr = {
+      source  = "vultr/vultr"
+      version = "2.12.0"
+    }
+  }
 }
 
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+}
+
+provider "vultr" {
+  api_key    = var.vultr_api_key
+  rate_limit = 100
 }
